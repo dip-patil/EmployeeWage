e28ab196fada6bdb31755c97a326a82477dff9a4
dipkumar patil <dipkumarpa2020@gmail.com>
2024-10-08 14:34:49 +0530

    Calculate Wages till a condition of total working hours of 100 or max days os 20 is reached for a month

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -12,17 +12,17 @@ namespace Employee_Wage
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NO_OF_WORKING_DAY = 20;
-        static void Main(string[] args)
+        public const int MAX_HRS_IN_MONTH = 10;
+        
+static void Main(string[] args)
         {
-
-            int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            for (int day = 0; day < NO_OF_WORKING_DAY; day++)
+            
+            int empHrs= 0, totalEmpHrs= 0, totalWorkingDays = 0;
+           
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAY)
             {
-
+                totalWorkingDays++;
                 Random random = new Random();
-
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
@@ -36,12 +36,11 @@ namespace Employee_Wage
                         empHrs = 0;
                         break;
                 }
-
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage:" + empWage);
-            }
-            Console.WriteLine("Total Employee Wage:" + totalEmpWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day# : " + totalWorkingDays + " Emp Hrs: " + empHrs);
+             }
+            int totalEmpWage= totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage:" + totalEmpWage);
             Console.ReadLine();
         }
         
