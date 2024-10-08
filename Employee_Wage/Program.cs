c87426a742b45a352c1e77d1c28d26357b065725
dipkumar patil <dipkumarpa2020@gmail.com>
2024-10-08 12:37:19 +0530

    Calculating Wages for a Month assuming 20 Working Days in a Month

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -11,29 +11,37 @@ namespace Employee_Wage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAY = 2;
         static void Main(string[] args)
         {
 
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            
-            int empCheck = random.Next(0, 3);
-            switch(empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NO_OF_WORKING_DAY; day++)
             {
-                case IS_PART_TIME :
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME :
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+
+                Random random = new Random();
+
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage:" + empWage);
             }
-           
-            empWage = empHrs*EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage:" + empWage);
+            Console.WriteLine("Total Employee Wage:" + totalEmpWage);
             Console.ReadLine();
         }
     }
