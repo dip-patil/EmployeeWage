d88b30136ed55ee6eaa2fededc285882c919152a
dipkumar patil <dipkumarpa2020@gmail.com>
2024-10-08 14:55:30 +0530

    Compute Employee Wage for Multiple Company in Procedural Way Using Class Methods

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -10,47 +10,45 @@ namespace Employee_Wage
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NO_OF_WORKING_DAY = 20;
-        public const int MAX_HRS_IN_MONTH = 10;
-        
 
-        public static int ComputeEmpWage()
-          {
-
-                int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-
-                while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAY)
+        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            
+            int empHrs= 0, totalEmpHrs = 0, totalWorkingDays = 0;
+          
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random= new Random();
+                int empCheck =random.Next(0, 3);
+                switch (empCheck)
                 {
-                    totalWorkingDays++;
-                    Random random = new Random();
-                    int empCheck = random.Next(0, 3);
-                    switch (empCheck)
-                    {
-                        case IS_PART_TIME:
-                            empHrs = 4;
-                            break;
-                        case IS_FULL_TIME:
-                            empHrs = 8;
-                            break;
-                        default:
-                            empHrs = 0;
-                            break;
-                    }
-                    totalEmpHrs += empHrs;
-                    Console.WriteLine("Day# : " + totalWorkingDays + " Emp Hrs: " + empHrs);
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
                 }
-                int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Total Emp Wage:" + totalEmpWage);
+               
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+            }
+            int totalEmpWage =totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company " + company + " is: " +totalEmpWage);
             return totalEmpWage;
-
         }
+        
         static void Main(string[] args)
         {
-            ComputeEmpWage();
+            ComputeEmpWage("DMart", 20, 2, 10);
+            ComputeEmpWage("Reliance", 10, 4, 20);
             Console.ReadLine();
         }
-        
+
     }
 }
 
